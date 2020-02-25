471822a5ccc0fa62cb2c334cf66f1880a1276bc8
İsmail CEYLAN <ismaillceylan@gmail.com>
2020-02-25 17:43:43 +0300

    Create type.js

---
+++ type.js
@@ -0,0 +1,446 @@
+/**
+ * Tür oluşturur.
+ * @param {String} name tür adı
+ */
+var Type = function( name )
+{
+	if( ! ( this instanceof Type ))
+
+		return new Type( name );
+
+	if( ! typeof( name ) == "string" || ! /^[a-z_$]{1}[a-z0-9_$]*$/i.test( name ))
+
+		throw TypeError( "To define a type, the first argument should take a valid native function name." );
+
+	/**
+	 * Türün adı.
+	 * @type {String}
+	 */
+	this.name = name[ 0 ].toUpperCase() + name.slice( 1 );
+
+	/**
+	 * Bu türün kendi türü de dahil sahip olduğu türleri tutar.
+	 * @type {Array}
+	 */
+	this.types = [ this.name ];
+
+	/**
+	 * Bu türün miras aldığı trait isimlerini tutar.
+	 * @type {Array}
+	 */
+	this.behaviours = [];
+
+	/**
+	 * Bu türün miras aldığı ebeveyn tür.
+	 * @type {Type}
+	 */
+	this.parent = null;
+
+	/**
+	 * Bu türden singleton yoluyla üretilen ilk instance.
+	 * @type {Object}
+	 */
+	this.instance = null;
+
+	/**
+	 * Türü native olarak temsil edecek kurucu metot.
+	 * @type {Function}
+	 */
+	this.constructor = eval( "( function " + this.name + "(){})" );
+
+	/**
+	 * Verilen trait'(ler)in özelliklerini bu türün özellikleri arasına
+	 * kopyalar. Özellikler bu türün kurucusunun prototype'ına kopyalanır
+	 * yani kopyalanan özellikler sanki bu türün birer özelliğiymiş gibi görünür.
+	 * 
+	 * @param {Trait} ...parents özellikleri miras alınacak trait(ler)
+	 * @return {this}
+	 */
+	this.use = function( parents )
+	{
+		Array.prototype.slice.call( arguments ).forEach( function( trait )
+		{
+			this.renameTraitConstructMethod( trait );
+			this.extendPrototype( trait.properties );
+
+			this.behaviours = this.behaviours.concat( trait.types );
+		},
+		this );
+
+		return this;
+	}
+
+	/**
+	 * Verilen trait'in özellikleri arasındaki construct metodunu benzersiz
+	 * bir isimle değiştirir. Böylece türlere ait construct metotları ile
+	 * çakışmaların önüne geçilir.
+	 * 
+	 * @param {Trait} trait bir trait
+	 */
+	this.renameTraitConstructMethod = function( trait )
+	{
+		if( trait.properties.construct )
+		{
+			trait.properties[ "construct" + trait.name + "Trait" ] = trait.properties.construct;
+			delete trait.properties.construct;
+		}
+	}
+
+	/**
+	 * Tür prototype'ını oluşturur.
+	 * 
+	 * @param {Object} context türün metot ve özelliklerini içeren bir nesne
+	 * @return {this}
+	 */
+	this.prototype = function( context )
+	{
+		this.extendPrototype( context );
+
+		if( this.parent )
+		
+			this.inherit( this.parent.constructor.prototype );
+
+		this.renameTraitMethods();
+		this.upgradePrototype();
+
+		return this;
+	}
+
+	/**
+	 * Verilen nesnedeki özellikleri türün prototype yığınına ekler.
+	 * 
+	 * @param {Object} context metot ve özellikler içeren bir nesne
+	 * @return {this}
+	 */
+	this.extendPrototype = function( context )
+	{
+		Object.assign( this.constructor.prototype, context );
+		return this;
+	}
+
+	/**
+	 * Temsil edilen türün miras aldığı trait'lerde bulunan metotlar,
+	 * tür prototype'ı tanımlanırken yeniden isimlendirilmiş olabilir.
+	 * Bunları uygular.
+	 */
+	this.renameTraitMethods = function()
+	{
+		var proto = this.constructor.prototype;
+
+		// kurucu prototype'ında use isimli bir property yoksa işlem yok
+		if( ! proto.hasOwnProperty( "AS" ))
+
+			return;
+
+		var AS = proto.AS;
+
+		for( var oldMethodName in AS )
+		{
+			var newMethodName = AS[ oldMethodName ];
+
+			proto[ newMethodName ] = proto[ oldMethodName ];
+			delete proto[ oldMethodName ];
+		}
+
+		delete proto.AS;
+	}
+
+	/**
+	 * Verilen üstel türün özelliklerini bu türün özellikleri arasına
+	 * kopyalar, miras alır.
+	 * 
+	 * @param {Type} parent özellikleri miras alınacak ebeveyn tür
+	 * @return {this}
+	 */
+	this.extends = function( parent )
+	{
+		this.parent = parent;
+		this.types = this.types.concat( parent.types );
+		this.behaviours = this.behaviours.concat( parent.behaviours );
+
+		return this;
+	}
+
+	/**
+	 * Temsil edilen türün kurucu metodunu verilen parametrelerle
+	 * örnekleyip döndürür.
+	 * 
+	 * @return {Object}
+	 */
+	this.new = function()
+	{
+		var instance = new this.constructor;
+
+		this.callTraitInitializers( instance );
+		this.igniteConstructMethod( instance, arguments );
+		
+		return instance;
+	}
+
+	/**
+	 * Verilen nesne içindeki property'ler bu türün
+	 * prototype'ında yoksa buraya yerleştirilir. Tür
+	 * o property'e sahipse bunu kullanmaya devam eder.
+	 * 
+	 * @param {Object} context miras alınacakları içeren bir nesne
+	 */
+	this.inherit = function( context )
+	{
+		var target = this.constructor.prototype;
+
+		for( var prop in context )
+		{
+			var value = context[ prop ];
+
+			if( prop in target )
+
+				continue;
+
+			target[ prop ] = value;
+		}
+	}
+
+	/**
+	 * Verilen nesnenin miras aldığı davranışların kendi iç işlerini
+	 * ilgilendiren kurucu metotları varsa bunları çalıştırır.
+	 * 
+	 * @param {Object} instance bir tür
+	 */
+	this.callTraitInitializers = function( instance )
+	{
+		instance.type.behaviours.forEach( function( behaviour )
+		{
+			var initializer;
+
+			if( initializer = instance[ "construct" + behaviour + "Trait" ])
+			
+				initializer.call( instance );
+		});
+	}
+
+	/**
+	 * Verilen nesne üzerinde construct adında bir metot varsa buna
+	 * verilen arguments nesnesindeki argümanları geçirerek çalıştırır.
+	 * Metot kendi etki alanında çalışır yani construct metodunun
+	 * içinde kullanlacak this sözcüğü o metodun içinde bulunduğu türe
+	 * refere eder.
+	 * 
+	 * @param {Object} instance bir nesne
+	 * @param {Arguments} initialArgs kurulum anındaki argüman listesi
+	 */
+	this.igniteConstructMethod = function( instance, initialArgs )
+	{
+		is( instance.construct, "function" ) &&
+			instance.construct.apply( instance, Array.prototype.slice.call( initialArgs ));
+	}
+
+	/**
+	 * Temsil edilen türün kurucu metodunu verilen parametrelerle
+	 * örnekleyip döndürür. new metodundan farkı her çalıştırıldığında
+	 * yeni bir instance üretmek yerine hep aynı instance'ı döndürür.
+	 * 
+	 * @return {Object}
+	 */
+	this.singleton = function()
+	{
+		if( this.instance )
+
+			return this.instance;
+
+		return this.instance = this.new.apply( this, Array.prototype.slice.call( arguments ));
+	}
+
+	/**
+	 * Temsil edilen türün prototype'ına gerekli özellikleri yerleştirir.
+	 */
+	this.upgradePrototype = function()
+	{
+		this.extendPrototype(
+		{
+			// bu kurucuyu oluşturan tür arayüzüne
+			// instance'lar üzerinden ulaşabilelim
+			type: this,
+
+			// instance'lar üzerinde tür sınama metodu bulunsun
+			// bu metot instanceof işleyişinin yerini alacak
+			is: this.is,
+
+			// instance'lar üzerinde davranış sınama metodu bulunsun
+			behave: this.behave,
+
+			// super metodu bulunsun
+			super: this.super
+		});
+	}
+
+	/**
+	 * Temsil edilen türün verilen türü miras alıp almadığını söyler.
+	 * 
+	 * @param {Type} target sınanacak bir tür veya tür adı
+	 * @return {Boolean}
+	 */
+	this.is = function( target )
+	{
+		if( target instanceof Type )
+
+			target = target.name;
+
+		if( this instanceof Type )
+
+			return this.types.indexOf( target ) > -1;
+		
+		if( this.type && this.type instanceof Type )
+			
+			return this.type.types.indexOf( target ) > -1;
+	}
+
+	/**
+	 * Temsil edilen türün verilen trait'i miras alıp almadığını söyler.
+	 * 
+	 * @param {Trait|String} target sınanacak bir trait veya trait adı
+	 * @return {Boolean}
+	 */
+	this.behave = function( trait )
+	{
+		if( trait instanceof Trait )
+
+			trait = trait.name;
+
+		if( this instanceof Type )
+
+			return this.behaviours.indexOf( trait ) > -1;
+		
+		if( this.type && this.type instanceof Type )
+			
+			return this.type.behaviours.indexOf( trait ) > -1;
+	}
+
+	/**
+	 * Temsil edilen türün ebeveyn türüne erişimi sağlar.
+	 *
+	 * - Ebeveynin belli bir metodunu call etmek
+	 * (String, [...Array])
+	 *
+	 * - Ebeveynin construct metodunu call etmek
+	 * (...Params)
+	 *
+	 * - Ebeneynin bağlamına erişmek
+	 * ()
+	 * 
+	 * @return {mixed}
+	 */
+	this.super = function()
+	{
+		var context = is( this, Type )
+			? this.parent
+			: this.type.parent;
+
+		if( ! context )
+
+			return;
+
+		context = context.constructor.prototype;
+
+		// parent bağlamına erişim
+		if( arguments.length == 0 )
+
+			return context;
+
+		// varsayacağımız metot adı construct olacak
+		var method = "construct";
+		// parametrelere dizi olarak ihtiyacımız var
+		var arg = Array.prototype.slice.call( arguments );
+
+		// adı verilmiş bir metot call edilecek
+		if( arg.length == 2 &&
+			typeof( arg[ 0 ]) == "string" &&
+			Object.prototype.toString.call( arg[ 1 ]) == "[object Array]"
+		)
+		{
+			// ilk parametre metot adı
+			method = arg.shift();
+			arg = arg.shift();
+		}
+		else if( arg.length == 1 && typeof( arg[ 0 ]) == "string" )
+
+			method = arg.shift();
+
+		// metot adı ve parametreler elimizde, call edelim
+		return context[ method ].apply( this, arg );
+	}
+}
+
+/**
+ * Trait oluşturur.
+ * @param {String} name trait adı
+ */
+var Trait = function( name )
+{
+	if( ! ( this instanceof Trait ))
+
+		return new Trait( name );
+
+	if( ! typeof( name ) == "string" || ! /^[a-z_$]{1}[a-z0-9_$]*$/i.test( name ))
+
+		throw TypeError( "İlk argüman geçerli bir davranışsal isim olmalıdır!" );
+
+	/**
+	 * Trait'in adı.
+	 * @type {String}
+	 */
+	this.name = name[ 0 ].toUpperCase() + name.slice( 1 );
+
+	/**
+	 * Bu trait'in kendi trait'i de dahil sahip olduğu trait'leri tutar.
+	 * @type {Array}
+	 */
+	this.types = [ this.name ];
+
+	/**
+	 * Trait'in kendininki ve miras aldığı trait'lerdeki metot ve özellikler.
+	 * @type {Object}
+	 */
+	this.properties = {}
+
+	/**
+	 * Verilen üstel trait'(ler)in özelliklerini bu trait'in özellikleri arasına
+	 * kopyalar, miras alır.
+	 * 
+	 * @param {Trait} ...parents özellikleri miras alınacak ebeveyn trait(ler)
+	 * @return {this}
+	 */
+	this.use = function( parents )
+	{
+		Array.prototype.slice.call( arguments ).forEach( function( parent )
+		{
+			Object.assign( this.properties, parent.properties );
+			this.types = this.types.concat( parent.types );
+		},
+		this );
+
+		return this;
+	}
+
+	/**
+	 * Verilen nesnedeki özellikleri trait'in prototype yığınına ekler.
+	 * 
+	 * @param {Object} context trait'in metot ve özelliklerini içeren bir nesne
+	 * @return {this}
+	 */
+	this.prototype = function( context )
+	{
+		Object.assign( this.properties, context );
+		return this;
+	}
+
+	/**
+	 * Temsil edilen trait'in verilen türü miras alıp almadığını söyler.
+	 * 
+	 * @param {Trait} target sınanacak bir trait
+	 * @return {Boolean}
+	 */
+	this.is = function( target )
+	{
+		return this.types.indexOf( target.name ) > -1;
+	}
+}
