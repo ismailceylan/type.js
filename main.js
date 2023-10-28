86848e80df3de5de2babbbce9d05a56a1ac3a31a
Ismail Ceylan <ismaillceylan@gmail.com>
2023-10-28 15:22:26 +0300

    refactor: testings

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-import { Type } from "./src/index.js";
+import { Type, Trait } from "./src/index.js";
 
-const foo = Type( "haha")
+const Breathable = Trait( "Breathable" ).prototype(
+{
+	breath()
+	{
+		console.log( "breathing" );
+	}
+});
 
-console.log(foo);
+const Creature = Type( "Creature" ).prototype(
+{
+	live()
+	{
+		console.log( "im living" );
+	}
+});
+
+const Animal = Type( "Animal" ).extends( Creature ).use( Breathable ).prototype(
+{
+	walk()
+	{
+		console.log( "walking" );
+	}
+});
+
+const Human = Type( "Human" ).extends( Animal ).prototype(
+{
+	talk()
+	{
+		console.log( "talking" );
+	}
+});
+
+console.log( Human.new( "ok" ).breath() );
