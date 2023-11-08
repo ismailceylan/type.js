922781005747f24a46b74da2c4152c7fea30f581
Ismail Ceylan <ismaillceylan@gmail.com>
2023-11-08 12:47:11 +0300

    fix: regex improvements & fixings

--- src/utils/get-arguments.js
+++ src/utils/get-arguments.js
@@ -1,7 +1,7 @@
 export default function getArguments( fnString )
 {
 	var matches = fnString.trim().match(
-		/^(?:async\s+)?(?:function\s+[\w@$]+\s*\((.*?)\)\s*{|function\s+\((.*?)\)\s*{|[\w@$]+\s*\((.*?)\)\s*{|\(?(.*?)\)?\s*=>)/
+		/^(?:async\s+)?(?:function\s+[\w@$]+\s*\((.*?)\)\s*{|function\s*\((.*?)\)\s*{|[\w@$]+\s*\((.*?)\)\s*{|\(?(.*?)\)?\s*=>{?)/
 	);
 
 	var match = ( matches[ 1 ] || matches[ 2 ] || matches[ 3 ] || matches[ 4 ]);
