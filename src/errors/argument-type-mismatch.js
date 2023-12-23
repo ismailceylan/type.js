cab18acd74bb1d8de4339797c49439cc99c2273c
Ismail Ceylan <ismaillceylan@gmail.com>
2023-12-23 14:23:38 +0300

    refactor(errors): remove unnecessary message

--- src/errors/argument-type-mismatch.js
+++ src/errors/argument-type-mismatch.js
@@ -7,25 +7,7 @@ export default class ArgumentTypeMismatch extends Error
 		super();
 
 		this.name = "ArgumentTypeMismatch";
-		this.iface = iface;
-		this.type = type;
-		this.methodRule = methodRule;
-		this.argRule = argRule;
-		this.position = position;
-		this.value = value;
 
-		this.message = argRule.types.length === 0
-			? this.argumentShouldPassMessage()
-			: this.argumentTypeMismatchMessage();
-	}
-
-	argumentTypeMismatchMessage()
-	{
-		return `${ this.iface.name } interface requires ${ this.type.name }.${ this.methodRule.name } method's ${ this.position }st argument (#${ this.argRule.name }) must be ${ readableJoin( typeNames( this.argRule.types )).toLowerCase()} but received ${ typeName( this.value ).toLowerCase()}.`;
-	}
-
-	argumentShouldPassMessage()
-	{
-		return `The ${ this.iface.name } interface prevents calling the ${ this.type.name }.${ this.methodRule.name } method without defining the ${ this.position }th argument (#${ this.argRule.name }).`;
+		this.message = `${ iface.name } interface requires ${ type.name }.${ methodRule.name } method's ${ position }st argument (#${ argRule.name }) must be ${ argRule.types.length === 0? "defined" : readableJoin( typeNames( argRule.types )).toLowerCase()} but received ${ typeName( value ).toLowerCase()}.`;
 	}
 }
