441e419668c06c4e5c30de218bff5ce95ac9cb6a
Ismail Ceylan <ismaillceylan@gmail.com>
2023-12-27 17:24:04 +0300

    refactor: optimization

--- src/utils/allowed.js
+++ src/utils/allowed.js
@@ -2,9 +2,18 @@ import { typeName } from "./index.js";
 
 export default function allowed( value, types )
 {
-	for( const type of types )
+	const valueTag = typeName( value );
+
+	for( let type of types )
 	{
-		if( typeName( value ) == type.prototype.constructor.name )
+		let typeTag = typeName( type );
+
+		if( typeTag == "Function" )
+		{
+			typeTag = type.prototype.constructor.name;
+		}
+
+		if( valueTag == typeTag )
 		{
 			return true;
 		}
